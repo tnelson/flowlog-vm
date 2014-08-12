d910d92e774a16ed77a10a691821231961bce0ef
Tim Nelson <tn@cs.wpi.edu>
2014-08-12 12:12:49 -0400

    protobufs note

--- install-racket.sh
+++ install-racket.sh
@@ -3,4 +3,6 @@ echo "deb http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --a
 echo "deb-src http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --append /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get -y install racket
-# this will be the latest version, though? Need <6 in order to use protobufs library
+
+# this will be the latest version, which has a hiccup when installing the protobufs library
+# May need to run this in racket *before* building dependencies for IOS parser (require (planet murphy/protobuf:1:1))
