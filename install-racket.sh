20b6ab9d9c00205fe1d22ef1c7fc496ef7b377b9
Tim Nelson <tn@cs.brown.edu>
2014-08-15 09:46:49 -0400

    fix racket script

--- install-racket.sh
+++ install-racket.sh
@@ -1,6 +1,26 @@
 # Add .deb repo for newer versions of racket
-echo "deb http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --append /etc/apt/sources.list
-echo "deb-src http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --append /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get -y install racket
-# this will be the latest version, though? Need <6 in order to use protobufs library
+#echo "deb http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --append /etc/apt/sources.list
+#echo "deb-src http://ppa.launchpad.net/plt/racket/ubuntu trusty main" | sudo tee --append /etc/apt/sources.list
+#sudo apt-get update
+#sudo apt-get -y install racket
+
+# Latest version has issues with protobuf library. Instead, use 5.3.6 for IOS processing:
+
+cd
+wget http://mirror.racket-lang.org/installers/5.3.6/racket/racket-5.3.6-src-unix.tgz
+tar xfz racket-5.3.6-src-unix.tgz
+cd racket-5.3.6
+cd src
+mkdir build
+cd build
+../configure
+make
+make install
+
+echo -e "\n# Racket path" >> ~/.profile
+echo -e 'PATH="$HOME/racket-5.3.6/bin:$PATH"' >> ~/.profile
+source ~/.profile
+
+cd
+rm racket-5.3.6-src-unix.tgz
+
