a8fe14537bdc03bed7c45ae93153570f88c7501e
Tim Nelson <tn@cs.brown.edu>
2014-09-10 11:36:10 -0400

    .

--- disable-ipv6.sh
+++ disable-ipv6.sh
@@ -1,4 +1,7 @@
 # Only works until system restart
-sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
-sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
+#sudo sysctl -w net.ipv6.conf.all.disable_ipv6=1
+#sudo sysctl -w net.ipv6.conf.default.disable_ipv6=1
 
+# changes in sysctl.conf persist
+sudo sh -c 'echo "\n#Disable IPv6 to avoid discovery packets in mininet\nnet.ipv6.conf.all.disable_ipv6=1\nnet.ipv6.conf.default.disable_ipv6=1" >> /etc/sysctl.conf'
+sudo sysctl -p
