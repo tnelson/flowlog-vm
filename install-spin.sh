541583b195ffdf32622050dcc5e71961dc20036a
Tim Nelson <tn@cs.brown.edu>
2015-04-08 19:51:14 -0400

    .

--- install-spin.sh
+++ install-spin.sh
@@ -1,15 +1,18 @@
 sudo apt-get -y install byacc flex
 
+# newer version:
+# http://spinroot.com/spin/Src/spin643.tar.gz
+
 cd
-wget http://spinroot.com/spin/Src/spin632.tar.gz
-tar xzf spin632.tar.gz
-cd Spin/Src6.3.2
+wget http://spinroot.com/spin/Src/spin643.tar.gz
+tar xzf spin643.tar.gz
+cd Spin/Src6.4.3
 make
 
 cd
-rm spin632.tar.gz
+rm spin643.tar.gz
 
 echo -e "\n# Spin path" >> ~/.profile
-echo -e 'PATH="$HOME/Spin/Src6.3.2:$PATH"' >> ~/.profile
+echo -e 'PATH="$HOME/Spin/Src6.4.3:$PATH"' >> ~/.profile
 
 source ~/.profile
